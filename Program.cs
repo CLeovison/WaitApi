34efcb3ec002f8747356e9f0f3bb17852c17e4da
Clark Leovison Rey <cleovison@gmail.com>
2024-11-11 00:24:55 +0800

    feat: FastEndpoints / Minimal Api Migration for Wait

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using FastEndpoints;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+app.UseFastEndpoints();
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Install This Packages
+
+dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
+dotnet add package Npgsql
+dotnet add FastEndpoints
