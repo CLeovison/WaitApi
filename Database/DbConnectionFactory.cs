e25cf6cb2dc39dac59574a8ab9a0bc0a4a360deb
Clark Leovison Rey <cleovison@gmail.com>
2024-11-11 00:51:27 +0800

    feat: Adding DbConnectionFactory

---
+++ Database/DbConnectionFactory.cs
@@ -0,0 +1,22 @@
+using System.Data;
+using Npgsql;
+
+namespace WaitApi.Database;
+
+public interface IDbConnectionFactory{
+    public Task<IDbConnection> CreateConnectionAsync();
+}
+
+public class PostgresConnectionFactory : IDbConnectionFactory{
+    private readonly string _connectionString;
+
+    public PostgresConnectionFactory(string connectionString){
+        _connectionString = connectionString;
+    }
+
+    public async Task<IDbConnection> CreateConnectionAsync(){
+        var connection = new NpgsqlBatchCommand(_connectionString);
+        await connection.CanCreateParameter()
+
+    }
+}
