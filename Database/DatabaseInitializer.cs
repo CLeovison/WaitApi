c166158ebcfaa2ce149e5b484a00cf46bb696bbe
Clark Leovison Rey <cleovison@gmail.com>
2024-12-01 19:48:42 +0800

    feat: Configuring Database Connection

--- Database/DatabaseInitializer.cs
+++ Database/DatabaseInitializer.cs
@@ -2,21 +2,24 @@ using Dapper;
 
 namespace WaitApi.Database;
 
-public class DatabaseIntializer{
+public class DatabaseIntializer
+{
     private readonly IDbConnectionFactory _connectionFactory;
 
-    public DatabaseIntializer(IDbConnectionFactory connectionFactory){
+    public DatabaseIntializer(IDbConnectionFactory connectionFactory)
+    {
         _connectionFactory = connectionFactory;
     }
 
-    public async Task InitializeAsync(){
+    public async Task InitializeAsync()
+    {
         using var connection = await _connectionFactory.GetDbConnection();
         await connection.ExecuteAsync(@"CREATE TABLE IF NOT EXISTS User (
         Id CHAR(36) PRIMARY KEY, 
         Username TEXT NOT NULL,
         FullName TEXT NOT NULL,
         Email TEXT NOT NULL,
-        DateOfBirth TEXT NOT NULL)", @"CREATE TABLE IF NOT EXISTS Product ()");
+        Birthday TEXT NOT NULL)");
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
+using WaitApi.Database;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
 var app = builder.Build();  
 
 app.Run();
