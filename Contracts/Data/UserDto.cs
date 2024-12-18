3675120c994bd9d81f2f75fd3140c344d4d1a784
Clark Leovison Rey <cleovison@gmail.com>
2024-12-19 01:10:32 +0800

    feat: Added New Interface to Repositories

--- Contracts/Data/UserDto.cs
+++ Contracts/Data/UserDto.cs
@@ -1,1 +1,12 @@
 namespace WaitApi.Contracts.Data;
+
+
+public class UserDto
+{
+
+    public required string FullName { get; init; }
+    public DateTime Birthday { get; init; }
+    public required string Username { get; init; }
+    public required string Email { get; init; }
+    public DateTime CreatedAt { get; init; }
+}
--- Repositories/IUserRepositories.cs
+++ Repositories/IUserRepositories.cs
@@ -1,8 +1,11 @@
+using WaitApi.Contracts.Data;
+
 namespace WaitApi.Repositories;
 
 public interface IUserRepositories{
 
-    Task<bool> CreateAsync();
-
+    Task<bool> CreateUserAsync();
+    Task<IEnumerable<UserDto>> GetUserIdAsync();
+    Task<bool> DeleteUserAsync();
     
 }
