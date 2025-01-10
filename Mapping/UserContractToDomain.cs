d05dd78a9d16494facf5f0f14eed29f15b8d1c25
Clark Leovison Rey <cleovison@gmail.com>
2025-01-10 23:48:16 +0800

    feat: Implementing Manual Mapping

--- Mapping/UserContractToDomain.cs
+++ Mapping/UserContractToDomain.cs
@@ -1,6 +1,12 @@
+using WaitApi.Domain.UserDomain;
+
 namespace WaitApi.Mapping;
 
 
-public static class UserContractToDomain{
+public static class UserContractToDomain
+{
+    public static Users GetUsers()
+    {
 
+    }
 }
