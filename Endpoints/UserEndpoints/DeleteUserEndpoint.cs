852d2afdb4b9ab25f289add3b03cf90109d7902b
Clark Leovison Rey <cleovison@gmail.com>
2025-01-18 23:33:03 +0800

    feat: Added A DeleteUserEndpoint

---
+++ Endpoints/UserEndpoints/DeleteUserEndpoint.cs
@@ -0,0 +1,12 @@
+using WaitApi.Abstract;
+
+namespace WaitApi.Endpoints.UserEndpoints;
+
+
+public class DeleteUserEndpoint : IEndpoint{
+
+    public void Endpoint(IEndpointRouteBuilder app){
+
+    }
+
+}
